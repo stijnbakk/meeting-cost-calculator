de27aefe45ff3d8bde032688c1ed6d6dd7c21459
Stijn Bakker <stijn@stijnbakker.com>
2023-04-18 12:43:52 +0200

    add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,15 @@
 	import { locale, locales } from 'svelte-i18n';
 	import Settings from './Settings.svelte';
 	import { _ } from 'svelte-i18n';
+
+	$:console.log('selected locale: ', $locale);
 </script>
 
 <div class="flex flex-row gap-2 p-2">
 	<div class="flex-grow items-center flex md:pl-5">
 		<span class="text-sm opacity-50 block font-bold font-serif">{$_('app_title')}</span>
 	</div>
+	<p>{$locale}</p>
 	<select class="border-[1px] rounded border-black" bind:value={$locale}>
 		<option value="nl">🇳🇱</option>
 		<option value="en">🇬🇧</option>
